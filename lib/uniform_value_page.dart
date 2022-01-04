3b01878d7f975a44d1bcb64a66ba00736664a60b
Jason <jason.castillo42@gmail.com>
2022-01-04 13:45:35 -0800

    uniform page, modification of future page

---
+++ lib/uniform_value_page.dart
@@ -0,0 +1,246 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class UniformValue extends StatefulWidget {
+  const UniformValue({Key? key}) : super(key: key);
+
+  @override
+  _UniformValueState createState() => _UniformValueState();
+}
+
+class _UniformValueState extends State<UniformValue> {
+
+  final _formKey = GlobalKey<FormState>();
+
+  final presentController = TextEditingController();
+  final futureController = TextEditingController();
+  final rateController = TextEditingController();
+  final periodsController = TextEditingController();
+
+  @override
+  void dispose(){
+    presentController.dispose();
+    futureController.dispose();
+    rateController.dispose();
+    periodsController.dispose();
+    super.dispose();
+  }
+
+  double outPut =0;
+  double present  = 0;
+  double future = 0;
+  double rate = 0;
+  int periods = 0;
+
+  void updateText(){
+    setState(() {
+
+      if(presentController.text.isEmpty){present=0;}
+      else{present = double.parse(presentController.text);}
+      if(futureController.text.isEmpty){future=0;}
+      else{future = double.parse(futureController.text);}
+      rate = double.parse(rateController.text)/100;
+      periods = int.parse(periodsController.text);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Uniform Value',
+            style: TextStyle(fontSize: 30),
+          ),
+        ),
+        body: Center(
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Row(
+                    children: [
+                      Expanded(
+                          flex: 35,
+                          child: Container(
+                              margin: EdgeInsets.only(left: 20, right: 5),
+                              child: Text(
+                                'Present Value',
+                                style: TextStyle(
+                                  fontSize: 15,
+                                ),
+                              )
+                          )
+                      ),
+                      Expanded(
+                          flex: 50,
+                          child: Container(
+                            margin: EdgeInsets.only(right: 20, left: 5),
+                            child: TextFormField(
+                              textAlign: TextAlign.right,
+                              controller: presentController,
+                              //new InputDecoration ?
+                              decoration: InputDecoration(labelText: "pv"),
+                              keyboardType: TextInputType.number,
+                              inputFormatters: <TextInputFormatter>[
+                                FilteringTextInputFormatter.digitsOnly
+                              ],
+                            ),
+                          )
+                      )
+                    ]
+                ),
+                Row(
+                    children: [
+                      Expanded(
+                          flex: 35,
+                          child: Container(
+                              margin: EdgeInsets.only(left: 20, right: 5),
+                              child: Text(
+                                'Future value',
+                                style: TextStyle(
+                                  fontSize: 15,
+                                ),
+                              )
+                          )
+                      ),
+                      Expanded(
+                          flex: 50,
+                          child: Container(
+                            margin: EdgeInsets.only(right: 20, left: 5),
+                            child: TextFormField(
+                              textAlign: TextAlign.right,
+                              controller: futureController,
+                              decoration: InputDecoration(labelText: "fv"),
+                              keyboardType: TextInputType.number,
+                              inputFormatters: <TextInputFormatter>[
+                                FilteringTextInputFormatter.digitsOnly
+                              ],
+                            ),
+                          )
+                      )
+                    ]
+                ),
+                Row(
+                    children: [
+                      Expanded(
+                          flex: 35,
+                          child: Container(
+                              margin: EdgeInsets.only(left: 20, right: 5),
+                              child: Text(
+                                'Interest Rate',
+                                style: TextStyle(
+                                  fontSize: 15,
+                                ),
+                              )
+                          )
+                      ),
+                      Expanded(
+                          flex: 50,
+                          child: Container(
+                            margin: EdgeInsets.only(right: 20, left: 5),
+                            child: TextFormField(
+                              validator: (value){
+                                if(value!.isNotEmpty && double.parse(value) > 0){
+                                  return null;
+                                }
+                                else return 'required';
+                              },
+                              textAlign: TextAlign.right,
+                              controller: rateController,
+                              decoration: InputDecoration(labelText: "rate"),
+                              keyboardType: TextInputType.number,
+                              inputFormatters: <TextInputFormatter>[
+                                FilteringTextInputFormatter.digitsOnly
+                              ],
+                            ),
+                          )
+                      )
+                    ]
+                ),
+                Row(
+                    children: [
+                      Expanded(
+                          flex: 35,
+                          child: Container(
+                              margin: EdgeInsets.only(left: 20, right: 5),
+                              child: Text(
+                                'Periods',
+                                style: TextStyle(
+                                  fontSize: 15,
+                                ),
+                              )
+                          )
+                      ),
+                      Expanded(
+                          flex: 50,
+                          child: Container(
+                            margin: EdgeInsets.only(right: 20, left: 5),
+                            child: TextFormField(
+                              validator: (value){
+                                if(value!.isNotEmpty && double.parse(value) > 0){
+                                  return null;
+                                }
+                                else return 'required';
+                              },
+                              textAlign: TextAlign.right,
+                              controller: periodsController,
+                              decoration: InputDecoration(labelText: "nper"),
+                              keyboardType: TextInputType.number,
+                              inputFormatters: <TextInputFormatter>[
+                                FilteringTextInputFormatter.digitsOnly
+                              ],
+                            ),
+                          )
+                      )
+                    ]
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      flex: 30,
+                      child: Container(
+                        margin: EdgeInsets.only(left: 20, right: 5, top: 30),
+                        child: ElevatedButton(
+                            onPressed: (){
+                              if(_formKey.currentState!.validate()) {
+                                updateText();
+                                double P = present * ((rate * (pow(1 + rate, periods)))
+                                    /((pow(1 + rate, periods) - 1)));
+                                double F = future *
+                                    (rate / (pow(1 + rate, periods) - 1));
+                                outPut = P + F;
+                              }
+                            },
+                            child: Text(
+                              "ENTER",
+                            )
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                        flex: 70,
+                        child: Container(
+                          decoration: BoxDecoration(border: Border.all(width: 1)),
+                          margin: EdgeInsets.only(left: 5, right: 20, top: 30),
+                          child: Text(
+                            '${outPut.toStringAsFixed(2)}',
+                            textAlign: TextAlign.right,
+                            style: TextStyle(
+                              //backgroundColor: Colors.yellowAccent,
+                              fontSize: 20,
+                            ),
+
+                          ),
+                        )
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ));
+  }
+}
